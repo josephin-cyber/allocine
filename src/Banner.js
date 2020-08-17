dc27e7581d902518164ade89e443a6e15b605146
josephine kingombe <josephinekingombe11@gmail.com>
2020-08-17 13:43:15 +0200

    resolution https

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,28 @@
 import React, {useEffect, useState} from "react";
 import axios from "./axios";
 import requests from "./requests";
-import "./Banner.css"
+import "./Banner.css";
+import handleClick from './Row';
+import './Row.css'
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer"
 
 function Banner() {
     const [movie, setMovie] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("")
+
+    const handleClick = (movie)=> {
+        if(trailerUrl){
+            setTrailerUrl('');
+        } else{
+            movieTrailer(movie?.name || "")
+            .then(url=>{
+const urlParams = new URLSearchParams(new URL(url).search);
+setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error)=>console.log(error));
+        }
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -20,6 +38,14 @@ function Banner() {
     }, []);
 
     console.log(movie)
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
 
     function truncate(str, n){
         return str?.length > n? str.substr(0, n-1) + "..." : str;
@@ -39,19 +65,27 @@ function Banner() {
                 <h1 className="banner_title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
+
+                
                 <div className="banner_buttons">
 <button className="banner_button"
+                            onClick={()=>handleClick(movie)}
+                             >Jouer</button>
 
-
-
->Jouer</button>
 <button className="banner_button">Details</button>
                 </div>
                 <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
+
+            
+            
             <div className="banner_fadeBottom" />
+            
         </header>
+        
     )
+    
 }
+
 
 export default Banner;
